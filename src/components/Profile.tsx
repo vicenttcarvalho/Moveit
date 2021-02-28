90ccaf8eca24fd310b50a289a19997b873056218
vicenttcarvalho <vicentecleyton@gmail.com>
2021-02-27 23:25:43 -0300

    front-end moveit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/vicenttcarvalho.png" alt="Vicente Carvalho" />
+            <div>
+                <strong>Vicente Carvalho</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/global.css';
+
+import { ChallengesProvider } from '../contexts/ChallengeContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+      <Component {...pageProps} />
+  )
+}
+
+export default MyApp
